f8cea990471065f62e2a67d15bac6b722ae15fd1
dk6796 <daniel0kurniawan69@gmail.com>
2024-06-13 07:27:03 +0700

    create activity

---
+++ app/src/main/java/edu/bluejack23_2/demarj/activities/CreateActivity.kt
@@ -0,0 +1,11 @@
+package edu.bluejack23_2.demarj.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create)
+    }
+}
