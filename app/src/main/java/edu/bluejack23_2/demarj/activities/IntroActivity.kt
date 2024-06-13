f5bfe305c071baa3eb787e2c3a2e4576b3f526ad
dk6796 <daniel0kurniawan69@gmail.com>
2024-06-13 09:50:53 +0700

    coba register - 2

--- app/src/main/java/edu/bluejack23_2/demarj/activities/IntroActivity.kt
+++ app/src/main/java/edu/bluejack23_2/demarj/activities/IntroActivity.kt
@@ -1,12 +1,24 @@
 package edu.bluejack23_2.demarj.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import edu.bluejack23_2.demarj.R
+import edu.bluejack23_2.demarj.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.getStartedBttn.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/edu/bluejack23_2/demarj/activities/LoginActivity.kt
+++ app/src/main/java/edu/bluejack23_2/demarj/activities/LoginActivity.kt
@@ -1,12 +1,23 @@
 package edu.bluejack23_2.demarj.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import edu.bluejack23_2.demarj.R
+import edu.bluejack23_2.demarj.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toRegisterBttn.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
