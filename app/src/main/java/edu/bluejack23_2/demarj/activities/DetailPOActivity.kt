397a5924e3b072a09bb6d5d5ac84c2a7e7f11843
dk6796 <daniel0kurniawan69@gmail.com>
2024-06-18 12:45:16 +0700

    po detail page - 1

--- app/src/main/java/edu/bluejack23_2/demarj/activities/DetailPOActivity.kt
+++ app/src/main/java/edu/bluejack23_2/demarj/activities/DetailPOActivity.kt
@@ -2,16 +2,15 @@ package edu.bluejack23_2.demarj.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import edu.bluejack23_2.demarj.databinding.ActivityUpdatePoactivityBinding
+import edu.bluejack23_2.demarj.databinding.ActivityDetailPoactivityBinding
 
-class UpdatePOActivity : AppCompatActivity() {
+class DetailPOActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityUpdatePoactivityBinding
-
+    private lateinit var binding: ActivityDetailPoactivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityUpdatePoactivityBinding.inflate(layoutInflater)
+        binding = ActivityDetailPoactivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
